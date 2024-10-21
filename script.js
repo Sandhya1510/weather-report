abffca1c28185f6c830d37efdcf18917de96f621
Sandhya <sandhya.k@agilophiles.com>
2024-10-21 16:16:16 +0530

    adding javascript file

---
+++ script.js
@@ -0,0 +1,46 @@
+const apiKey = '90361915a9939baf51d746c901a893f1'; 
+
+async function fetchWeather() {
+    const city = document.getElementById('cityInput').value;
+    const weatherResult = document.getElementById('weatherResult');
+
+    // Clear previous results
+    weatherResult.innerHTML = '';
+
+    if (!city) {
+        alert("Please enter a city name.");
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+
+        if (!response.ok) {
+            throw new Error('City not found.');
+        }
+
+        const data = await response.json();
+        displayWeather(data, city);
+    } catch (error) {
+        weatherResult.innerText = `Error: ${error.message}`;
+    }
+}
+
+function displayWeather(data, city) {
+    const { main, weather, wind, clouds, sys} = data;
+    const weatherResult = document.getElementById('weatherResult');
+ 
+    let forecastHTML = `
+      <h2>Weather in ${city}</h2>
+      <h3>Country: ${sys.country}</h3>
+      <p>Temperature: ${main.temp}°C</p>
+      <p>Pressure: ${main.pressure} Pa</p>
+      <p>Humidity: ${main.humidity}%</p>
+      <p>Wind Speed: ${wind.speed} m/s</p>
+      <p>Clouds: ${clouds.all} 	(0~1)</p>
+      <p>Condition: ${weather[0].description}</p>
+
+    `;
+
+    weatherResult.innerHTML = forecastHTML;
+}
